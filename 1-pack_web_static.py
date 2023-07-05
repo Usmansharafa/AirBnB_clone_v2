4fe489d785ecf43557af83ef1f96f4b326f101c4
Usman Sharafa <Usmansharafa04@gmail.com>
2023-07-05 18:15:59 +0100

    Task 1 solved

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -13,8 +13,11 @@ def do_pack():
     """
     Generate a .tgz archive from the contents of the web_static folder
     """
-    local("mkdir -p versions")
-    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    filename = "versions/web_static_{}.tgz".format(timestamp)
-    local("tar -cvzf {} web_static".format(filename))
-    return filename
+    try:
+        local("mkdir -p versions")
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        filename = "versions/web_static_{}.tgz".format(timestamp)
+        local("tar -cvzf {} web_static".format(filename))
+        return filename
+    except Exception:
+        return None
