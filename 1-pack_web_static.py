03dc4272c5f1ac75bac3f8fec5ed50eedf65aafe
Usman Sharafa <Usmansharafa04@gmail.com>
2023-07-06 00:40:07 +0100

    Task 1 solved

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,6 +8,7 @@ from datetime import datetime
 
 env.hosts = ['localhost']
 
+
 def do_pack():
     """
     Generate a .tgz archive from the contents of the web_static folder
@@ -16,7 +17,6 @@ def do_pack():
         local("mkdir -p versions")
         timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
         filename = "versions/web_static_{}.tgz".format(timestamp)
-        print("Packing web_static to {}".format(filename[:-4]))
         local("tar -cvzf {} web_static".format(filename))
         return filename
     except Exception:
