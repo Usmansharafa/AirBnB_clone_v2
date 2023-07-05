a977282dd48660e90b818f0906f228234877be03
Usman Sharafa <Usmansharafa04@gmail.com>
2023-07-05 21:18:44 +0100

    Task 2 solved

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,9 +19,13 @@ def do_deploy(archive_path):
         put(archive_path, "/tmp/{}".format(archive_name))
         run("mkdir -p /data/web_static/releases/{}/".format(
             archive_name[:-4]))
-        run("tar -xzf /tmp/{} --strip-components=1 -C /data/web_static/releases/{}/".format(
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
             archive_name, archive_name[:-4]))
         run("rm /tmp/{}".format(archive_name))
+        run("mv /data/web_static/releases/{}/web_static/* {}".format(
+                archive_name[:-4], new_dir))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(
+            archive_name[:-4]))
         new_dir = "/data/web_static/releases/{}/".format(archive_name[:-4])
         run("rm -rf /data/web_static/current")
         ln_dir = "/data/web_static/current"
