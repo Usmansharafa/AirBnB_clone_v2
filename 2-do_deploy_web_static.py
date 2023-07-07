a5ac73a160a8eef22ebbcf3343db555657e3c60e
Usman Sharafa <Usmansharafa04@gmail.com>
2023-07-07 03:28:17 +0100

    Task 2 solved

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,22 +15,22 @@ def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
     try:
-        archive_name = archive_path.split("/")[-1].split(".")[0]
-        put(archive_path, "/tmp/")
+        archive_name = os.path.basename(archive_path)
+        put(archive_path, "/tmp/{}".format(archive_name))
         run("mkdir -p /data/web_static/releases/{}/".format(
-            archive_name))
-        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".format(
-            archive_name, archive_name))
-        run("rm -rf /tmp/{}.tgz".format(archive_name))
-        new_dir = "/data/web_static/releases/{}/".format(archive_name)
+            archive_name[:-4]))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
+            archive_name, archive_name[:-4]))
+        run("rm /tmp/{}".format(archive_name))
+        new_dir = "/data/web_static/releases/{}/".format(archive_name[:-4])
         run("mv /data/web_static/releases/{}/web_static/* {}".format(
-                archive_name, new_dir))
+                archive_name[:-4], new_dir))
         run("rm -rf /data/web_static/releases/{}/web_static".format(
-            archive_name))
+            archive_name[:-4]))
         run("rm -rf /data/web_static/current")
         ln_dir = "/data/web_static/current"
         run("ln -s /data/web_static/releases/{}/ {}".format(
-             archive_name, ln_dir))
+             archive_name[:-4], ln_dir))
         return True
     except Exception:
         return False
