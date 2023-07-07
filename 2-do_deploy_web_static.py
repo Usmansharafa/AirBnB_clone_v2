9bba65c37148475dc7ad85c34dfebd1ce07ea843
Usman Sharafa <Usmansharafa04@gmail.com>
2023-07-07 03:23:55 +0100

    Task 2 solved

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from fabric.api import env, local, run, put
 import os
 
-env.hosts = ['107.22.143.53', '18.210.14.41']
+env.hosts = ['35.153.83.147', '52.201.158.242']
 env.user = 'ubuntu'
 
 
@@ -15,22 +15,22 @@ def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
     try:
-        archive_name = os.path.basename(archive_path)
-        put(archive_path, "/tmp/{}".format(archive_name))
+        archive_name = archive_path.split("/")[-1].split(".")[0]
+        put(archive_path, "/tmp/")
         run("mkdir -p /data/web_static/releases/{}/".format(
-            archive_name[:-4]))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
-            archive_name, archive_name[:-4]))
-        run("rm /tmp/{}".format(archive_name))
+            archive_name))
+        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".format(
+            archive_name, archive_name))
+        run("rm -rf /tmp/{}.tgz".format(archive_name))
+        new_dir = "/data/web_static/releases/{}/".format(archive_name)
         run("mv /data/web_static/releases/{}/web_static/* {}".format(
-                archive_name[:-4], new_dir))
+                archive_name, new_dir))
         run("rm -rf /data/web_static/releases/{}/web_static".format(
-            archive_name[:-4]))
-        new_dir = "/data/web_static/releases/{}/".format(archive_name[:-4])
+            archive_name))
         run("rm -rf /data/web_static/current")
         ln_dir = "/data/web_static/current"
         run("ln -s /data/web_static/releases/{}/ {}".format(
-             archive_name[:-4], ln_dir))
+             archive_name, ln_dir))
         return True
     except Exception:
         return False
